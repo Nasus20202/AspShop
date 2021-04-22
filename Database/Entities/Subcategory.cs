bed294e9cb5450727081563e2a04c1a6c8261907
Krzysztof Nasuta <krzysiek.com.pl@wp.pl>
2021-04-22 16:40:07 +0200

    Added Admin Panel index view and user info view

---
+++ Database/Entities/Subcategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopWebApp.Database.Entities
+{
+    public class Subcategory : Category
+    {
+        public string Tags { get; set; }
+
+
+    }
+}
