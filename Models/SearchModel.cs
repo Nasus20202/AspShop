a30440cc0f9b6c30c97157dbe6eae72ac445735f
Krzysztof Nasuta <krzysiek.com.pl@wp.pl>
2021-05-01 12:25:10 +0200

    Added product search

---
+++ Models/SearchModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopWebApp
+{
+    public class SearchModel : BaseViewModel
+    {
+        public SearchModel() : base() { }
+        public string SearchFor { get; set; }
+        public IList<Product> Products { get; set; }
+        public int Page { get; set; }
+        public int LastPage { get; set; }
+        public int Count { get; set; }
+    }
+}
