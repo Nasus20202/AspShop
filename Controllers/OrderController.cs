e0654aa75096cde6bb92c3d357058b1c34be8d66
Krzysztof Nasuta <krzysiek.com.pl@wp.pl>
2021-06-11 17:32:25 +0200

    Order From is now working

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -27,6 +27,17 @@ namespace ShopWebApp.Controllers
             }
             if (model.Order == null)
                 model.Order = new Order();
+            if (input.Order != null)
+                model.Order = input.Order;
+            if(User.Identity.IsAuthenticated)
+            {
+                model.Order.ClientName = model.Order.ClientName == null || model.Order.ClientName == string.Empty ? model.User.Name : model.Order.ClientName;
+                model.Order.ClientSurname = model.Order.ClientSurname == null || model.Order.ClientSurname == string.Empty ? model.User.Surname : model.Order.ClientSurname;
+                model.Order.ClientPhone = model.Order.ClientPhone == null || model.Order.ClientPhone == string.Empty ? model.User.Phone : model.Order.ClientPhone;
+                model.Order.ClientEmail = model.Order.ClientEmail == null || model.Order.ClientEmail == string.Empty ? model.User.Email : model.Order.ClientEmail;
+                model.Order.Address = model.Order.Address == null || model.Order.Address == string.Empty ? model.User.Address : model.Order.Address;
+            }
+
             if (input.Message != null && input.Message != string.Empty)
                 model.Message = input.Message;
             model.Title = "Twoje zamówienie";
@@ -36,7 +47,11 @@ namespace ShopWebApp.Controllers
         [ActionName("Index")]
         public IActionResult SubmitOrder(OrderModel input)
         {
-            input.Message = input.Order.ClientName;
+            input.Message = string.Empty; bool isValid = true;
+            if (input.Order.ShippingType < 1 || input.Order.ShippingType > 3) { 
+                input.Message += "Niepoprawna metoda dostawy\n"; isValid = false; }
+            if (input.Order.PaymentMethod < 1 || input.Order.PaymentMethod > 3) {
+                input.Message += "Niepoprawna metoda płatności\n"; isValid = false; }
             return Index(input);
         }
     }
